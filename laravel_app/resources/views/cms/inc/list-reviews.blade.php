d33cf00adf25a2649b26c4840d2b4992ed71fb4f
haim3307 <haim3307@gmai.com>
2018-07-05 20:15:37 +0300

    css fix 2

--- laravel_app/resources/views/cms/inc/list-reviews.blade.php
+++ laravel_app/resources/views/cms/inc/list-reviews.blade.php
@@ -1,6 +1,6 @@
 @foreach($reviews as $review)
     <div class="comment-head-dash clearfix mb-3">
-        @include('items.review-item')
+        @include('items.review-item',['colA'=>'3','colB'=>'9'])
         <div class="text-center">
             About <a href="{{url("cms/product/{$review->product->id}/edit")}}">{{$review->product->title}}</a>
         </div>
