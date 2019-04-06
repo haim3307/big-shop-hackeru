5c0111eca9edff844952188aa0842ece9112d045
haim3307 <haim3307@gmail.com>
2019-04-06 15:06:35 +0300

    master

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,11 +16,8 @@ class HttpsProtocol
     public function handle($request, Closure $next)
     {
         if (!$request->secure() && env('APP_ENV') === 'production') {
-
             return redirect()->secure($request->getRequestUri());
-
         }
-
         return $next($request);
     }
 }
