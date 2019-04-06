37330007acbd23c176199c50375d27839807c06b
haim3307 <haim3307@gmail.com>
2019-04-06 15:18:26 +0300

    master

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,6 +16,9 @@ class HttpsProtocol
     public function handle($request, Closure $next)
     {
         if (!$request->secure() && env('APP_ENV') === 'production') {
+            // for Proxies
+            Request::setTrustedProxies([$request->getClientIp()]);
+
             return redirect()->secure($request->getRequestUri());
         }
         return $next($request);
